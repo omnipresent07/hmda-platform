6cc2bb8a81f7c29a86968a1b4150ab658c3c0aba
bsharma <bsharma@healthstarinfo.com>
2019-11-19 20:35:49 -0500

    debugged issue with lender credits showing empty

--- irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
+++ irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
@@ -1,9 +1,12 @@
 package hmda.publication.lar
 
+import java.io.File
+import java.nio.file.Paths
+
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{FileIO, Flow, Sink, Source}
 import akka.util.ByteString
 import hmda.messages.submission.HmdaRawDataEvents.LineAdded
 import hmda.model.filing.lar.LoanApplicationRegister
@@ -18,22 +21,27 @@ object IrsDebug extends App {
 //  val persistenceId = s"HmdaRawData-549300JYXTZDSPJEPI44-2018-4"
 //  val persistenceId = s"HmdaRawData-2549006DVF3CPHSCHN79-2018-7"
 //  val persistenceId = s"HmdaRawData-549300XR0EY1M0FVG232-2018-1"
-  val persistenceId = s"HmdaRawData-549300CCELEPUO4TOE73-2018-24"
+  val persistenceId = s"HmdaRawData-549300ZIQ24V0C88AC41-2018-3"
   val source: Source[LineAdded, NotUsed] = eventsByPersistenceId(persistenceId)
     .collect {
       case evt: LineAdded => evt
     }
-
+  val file = Paths.get("549300ZIQ24V0C88AC41.txt")
   source
     .drop(1)
     .map(l => l.data)
     .map(ByteString(_))
     .via(framing("\n"))
-    .map(_.utf8String)
-    .map(_.trim)
+//    .map(_.utf8String)
+//    .map(_.trim)
 //    .map(s => LarCsvParser(s).getOrElse(LoanApplicationRegister()))
-    .runWith(Sink.foreach(println))
+//    .filter(_.loan.ULI == "549300HW662MN1WU8550151803611008")
+//    .runWith(Sink.foreach(println))
+//    .map(t => ByteString(t))
+//    .via(framing("\n"))
+    .runWith(FileIO.toPath(file))
+//    .runWith(Sink.foreach(println))
     .onComplete(_ => system.terminate())(
       scala.concurrent.ExecutionContext.global)
-
+//  FileIO.toPath(file)
 }

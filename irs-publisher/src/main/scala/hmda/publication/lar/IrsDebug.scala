c3e3e14658be0dc8434c835f2d07f28d11a31067
bsharma <bsharma@healthstarinfo.com>
2019-12-18 21:46:34 -0500

    trying to fetch parser affected lei

--- irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
+++ irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
@@ -9,6 +9,7 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Flow, Sink, Source}
 import akka.util.ByteString
 import hmda.messages.submission.HmdaRawDataEvents.LineAdded
+import hmda.messages.submission.SubmissionProcessingCommands.StartParsing
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.parser.filing.lar.LarCsvParser
 import hmda.query.HmdaQuery.eventsByPersistenceId
@@ -17,29 +18,36 @@ import hmda.util.streams.FlowUtils.framing
 object IrsDebug extends App {
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
+  //runMain hmda.publication.lar.IrsDebug
 //  val persistenceId = s"HmdaRawData-254900TJCZAM25V2WZ38-2018-9"
 //  val persistenceId = s"HmdaRawData-549300JYXTZDSPJEPI44-2018-4"
 //  val persistenceId = s"HmdaRawData-2549006DVF3CPHSCHN79-2018-7"
 //  val persistenceId = s"HmdaRawData-549300XR0EY1M0FVG232-2018-1"
-  val persistenceId = s"HmdaRawData-549300ZIQ24V0C88AC41-2018-3"
-  val source: Source[LineAdded, NotUsed] = eventsByPersistenceId(persistenceId)
+//  val persistenceId = s"HmdaRawData-549300ZIQ24V0C88AC41-2018-3"
+  val persistenceId = s"HmdaParserError-549300KNV94E4Y2HSK54-2019-1"
+//  val source: Source[LineAdded, NotUsed] = eventsByPersistenceId(persistenceId)
+//    .collect {
+//      case evt: LineAdded => evt
+//    }
+
+  val source: Source[StartParsing, NotUsed] = eventsByPersistenceId(persistenceId)
     .collect {
-      case evt: LineAdded => evt
+      case evt: StartParsing => evt
     }
-  val file = Paths.get("549300ZIQ24V0C88AC41.txt")
+  val file = Paths.get("549300KNV94E4Y2HSK54.txt")
   source
-    .drop(1)
-    .map(l => l.data)
+//    .drop(1)
+    .map(l => l.submissionId)
     .map(ByteString(_))
     .via(framing("\n"))
 //    .map(_.utf8String)
 //    .map(_.trim)
 //    .map(s => LarCsvParser(s).getOrElse(LoanApplicationRegister()))
 //    .filter(_.loan.ULI == "549300HW662MN1WU8550151803611008")
-//    .runWith(Sink.foreach(println))
+    .runWith(Sink.foreach(println))
 //    .map(t => ByteString(t))
 //    .via(framing("\n"))
-    .runWith(FileIO.toPath(file))
+//    .runWith(FileIO.toPath(file))
 //    .runWith(Sink.foreach(println))
     .onComplete(_ => system.terminate())(
       scala.concurrent.ExecutionContext.global)

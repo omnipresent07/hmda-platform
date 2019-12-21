5062ac314a90de74ca29d93a752310bf7f397584
bsharma <bsharma@healthstarinfo.com>
2019-12-21 10:27:53 -0500

    check invalid wire type

--- irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
+++ irs-publisher/src/main/scala/hmda/publication/lar/IrsDebug.scala
@@ -15,6 +15,8 @@ import hmda.parser.filing.lar.LarCsvParser
 import hmda.query.HmdaQuery.eventsByPersistenceId
 import hmda.util.streams.FlowUtils.framing
 
+import scala.util.{Failure, Success}
+
 object IrsDebug extends App {
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
@@ -24,32 +26,36 @@ object IrsDebug extends App {
 //  val persistenceId = s"HmdaRawData-2549006DVF3CPHSCHN79-2018-7"
 //  val persistenceId = s"HmdaRawData-549300XR0EY1M0FVG232-2018-1"
 //  val persistenceId = s"HmdaRawData-549300ZIQ24V0C88AC41-2018-3"
-  val persistenceId = s"HmdaParserError-549300KNV94E4Y2HSK54-2019-1"
-//  val source: Source[LineAdded, NotUsed] = eventsByPersistenceId(persistenceId)
-//    .collect {
-//      case evt: LineAdded => evt
-//    }
-
-  val source: Source[StartParsing, NotUsed] = eventsByPersistenceId(persistenceId)
+  val persistenceId = s"HmdaRawData-549300KNV94E4Y2HSK54-2019-1"
+  val source: Source[LineAdded, NotUsed] = eventsByPersistenceId(persistenceId)
     .collect {
-      case evt: StartParsing => evt
+      case evt: LineAdded => evt
     }
+
+//  val source: Source[StartParsing, NotUsed] = eventsByPersistenceId(persistenceId)
+//    .collect {
+//      case evt: StartParsing => evt
+//    }
   val file = Paths.get("549300KNV94E4Y2HSK54.txt")
   source
 //    .drop(1)
-    .map(l => l.submissionId)
+    .map(l => l.data)
     .map(ByteString(_))
     .via(framing("\n"))
-//    .map(_.utf8String)
-//    .map(_.trim)
+    .map(_.utf8String)
+    .map(_.trim)
 //    .map(s => LarCsvParser(s).getOrElse(LoanApplicationRegister()))
 //    .filter(_.loan.ULI == "549300HW662MN1WU8550151803611008")
-    .runWith(Sink.foreach(println))
-//    .map(t => ByteString(t))
-//    .via(framing("\n"))
-//    .runWith(FileIO.toPath(file))
 //    .runWith(Sink.foreach(println))
-    .onComplete(_ => system.terminate())(
-      scala.concurrent.ExecutionContext.global)
+    .map(t => ByteString(t))
+    .via(framing("\n"))
+    .runWith(FileIO.toPath(file))
+//    .runWith(Sink.foreach(println))
+//    .onComplete(_ => system.terminate())(
+//      scala.concurrent.ExecutionContext.global)
+    .onComplete {
+      case (Success(_))  => system.terminate()
+      case (Failure(ex)) => println(s"Error happened: ${ex}")
+    }(scala.concurrent.ExecutionContext.global)
 //  FileIO.toPath(file)
 }

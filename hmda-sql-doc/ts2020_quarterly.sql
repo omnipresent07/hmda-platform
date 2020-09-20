4490c85bfe487bedb331279511a6486f5199b0e4
bsharma <bsharma@healthstarinfo.com>
2020-09-20 09:50:11 -0400

    add new table for TS

---
+++ hmda-sql-doc/ts2020_quarterly.sql
@@ -0,0 +1,4 @@
+create table ts2020_q1 as select * from transmittalsheet2020 where true = false;
+create table ts2020_q2 as select * from transmittalsheet2020 where true = false;
+create table ts2020_q3 as select * from transmittalsheet2020 where true = false;
+
